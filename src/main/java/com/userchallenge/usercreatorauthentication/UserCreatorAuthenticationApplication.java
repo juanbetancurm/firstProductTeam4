343fea52f6a38db1c1c6ee75ff334392439d3404
Juan Betancur <juanj_betancurm@soy.sena.edu.co>
2023-06-08 22:15:12 -0500

    First Commit with project running, but without packages.

---
+++ src/main/java/com/userchallenge/usercreatorauthentication/UserCreatorAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.userchallenge.usercreatorauthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserCreatorAuthenticationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserCreatorAuthenticationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/userchallenge/usercreatorauthentication/UserCreatorAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.userchallenge.usercreatorauthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserCreatorAuthenticationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
