6ed84a88fde888eb8c6ea67b1d3402e9a247c789
Juan Betancur <juanj_betancurm@soy.sena.edu.co>
2023-06-08 22:42:23 -0500

    UserRepository is done!

---
+++ src/main/java/com/userchallenge/usercreatorauthentication/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.userchallenge.usercreatorauthentication.repositories;
+
+import com.userchallenge.usercreatorauthentication.models.UserModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserModel, Long> {
+}
