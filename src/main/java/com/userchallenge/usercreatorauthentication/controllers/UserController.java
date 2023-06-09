aad621ce55e7b7dd7e3d95e61972575f1c4f34ab
Juan Betancur <juanj_betancurm@soy.sena.edu.co>
2023-06-08 23:09:58 -0500

    POST request working for UserModel!

--- src/main/java/com/userchallenge/usercreatorauthentication/controllers/UserController.java
+++ src/main/java/com/userchallenge/usercreatorauthentication/controllers/UserController.java
@@ -4,17 +4,19 @@ import com.userchallenge.usercreatorauthentication.models.UserModel;
 import com.userchallenge.usercreatorauthentication.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(path = "api/users")
+@RequestMapping(path = "/api/users")
 public class UserController {
 
     @Autowired
     private UserService userService;
 
+    @PostMapping
     public UserModel saveUser(@Validated @RequestBody UserModel userModel){
         return userService.saveUser(userModel);
     }
